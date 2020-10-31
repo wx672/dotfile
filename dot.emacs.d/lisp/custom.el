4c7e8d0b685fed4e4a5d589d1ddf95ca3e0e7838
Wang Xiaolin <wx672ster@gmail.com>
2020-10-31 23:11:20 +0800

    update

--- dot.emacs.d/lisp/custom.el
+++ dot.emacs.d/lisp/custom.el
@@ -7,7 +7,7 @@
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#FDF6E3"])
  '(blink-cursor-mode nil)
  '(bookmark-save-flag 1)
- '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-browser-function 'browse-url-generic)
  '(browse-url-generic-program (executable-find "x-www-browser"))
  '(cal-tex-24 t)
  '(case-fold-search t)
@@ -18,27 +18,24 @@
  '(cua-auto-tabify-rectangles nil)
  '(cua-selection-mode t)
  '(current-language-environment "UTF-8")
- '(cursor-type (quote bar))
- '(custom-enabled-themes (quote (material-light)))
+ '(cursor-type 'bar)
+ '(custom-enabled-themes '(material))
  '(custom-safe-themes
-   (quote
-	("24714e2cb4a9d6ec1335de295966906474fdb668429549416ed8636196cb1441" "afd761c9b0f52ac19764b99d7a4d871fc329f7392dfc6cd29710e8209c691477" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
+   '("d4f8fcc20d4b44bf5796196dbeabec42078c2ddb16dcb6ec145a1c610e0842f3" "24714e2cb4a9d6ec1335de295966906474fdb668429549416ed8636196cb1441" "afd761c9b0f52ac19764b99d7a4d871fc329f7392dfc6cd29710e8209c691477" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default))
  '(debug-on-error nil)
- '(default-major-mode (quote org-mode) t)
+ '(default-major-mode 'org-mode t)
  '(dict-databases
-   (quote
-	("wn" "gcide" "foldoc" "jargon" "moby-tresaurus" "vera" "langdao-ce" "langdao-ec")))
+   '("wn" "gcide" "foldoc" "jargon" "moby-tresaurus" "vera" "langdao-ce" "langdao-ec"))
  '(dict-enable-key-bindings t nil (dict))
  '(dict-mode-update-key-bindings nil t)
  '(dict-noauth t)
  '(dict-original-server "localhost")
- '(dict-servers (quote ("localhost")))
+ '(dict-servers '("localhost"))
  '(dict-show-one-window t)
  '(dict-update-key-bindings nil t)
  '(dired-guess-shell-alist-user nil)
  '(dired-omit-extensions
-   (quote
-	("~" ".bin" ".lbin" ".a" ".ln" ".blg" ".bcf" ".run.xml" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".snm" ".out" ".nav" ".out" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyg" ".pyo" ".idx" ".lof" ".lot" ".glo" ".blg" ".bbl" ".cp" ".cps" ".fn" ".fns" ".ky" ".kys" ".pg" ".pgs" ".tp" ".tps" ".vr" ".vrs")))
+   '("~" ".bin" ".lbin" ".a" ".ln" ".blg" ".bcf" ".run.xml" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".snm" ".out" ".nav" ".out" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyg" ".pyo" ".idx" ".lof" ".lot" ".glo" ".blg" ".bbl" ".cp" ".cps" ".fn" ".fns" ".ky" ".kys" ".pg" ".pgs" ".tp" ".tps" ".vr" ".vrs"))
  '(dired-omit-files-p t t)
  '(dired-omit-mode t t)
  '(dired-recursive-copies t)
@@ -52,7 +49,7 @@
  '(doc-view-continuous nil)
  '(fci-rule-color "#383838")
  '(fill-column 90)
- '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(fringe-mode '(1 . 1) nil (fringe))
  '(global-auto-revert-mode t)
  '(global-font-lock-mode t nil (font-lock))
  '(global-hl-line-mode t nil (hl-line))
@@ -60,13 +57,12 @@
  '(helm-full-frame t)
  '(hi-lock-mode t t (hi-lock))
  '(inhibit-startup-screen t)
- '(ispell-extra-args (quote ("--reverse")))
+ '(ispell-extra-args '("--reverse"))
  '(ispell-local-dictionary "en_US")
  '(ispell-local-dictionary-alist
-   (quote
-	(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil
+   '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil
 	  ("-d" "en_US")
-	  nil utf-8))))
+	  nil utf-8)))
  '(linum-format "%d ")
  '(magit-auto-revert-mode nil)
  '(make-backup-files nil)
@@ -74,30 +70,32 @@
  '(menu-bar-mode nil)
  '(mode-line-in-non-selected-windows t)
  '(nrepl-message-colors
-   (quote
-	("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(org-agenda-files
-   (quote
-	("~/Documents/rsync/swfu/lab-backup/lab-notes.org" "~/Documents/rsync/ytxx14/accounting.org")))
+   '("~/Documents/rsync/swfu/lab-backup/lab-notes.org" "~/Documents/rsync/ytxx14/accounting.org"))
  '(org-ditaa-jar-path "/usr/bin/ditaa")
- '(org-time-stamp-custom-formats (quote ("<%m/%d/%y>" . "<%m/%d/%y>")))
+ '(org-time-stamp-custom-formats '("<%m/%d/%y>" . "<%m/%d/%y>"))
  '(package-selected-packages
-   (quote
-	(wgrep-helm gnuplot gnuplot-mode helm-unicode elpy markdown-mode outline-magic yasnippet-classic-snippets material-theme graphviz-dot-mode nov csv-nav wgrep-ag nasm-mode web-mode yasnippet-snippets async helm auctex diminish helm-config helm-command helm-files ascii-art-to-unicode pyim yatemplate sicp org-pdfview org-chinese-utils htmlize helm-gtags helm-c-yasnippet helm-ag exec-path-from-shell csv-mode cider-eval-sexp-fu use-package anzu)))
+   '(semi w3m wgrep-helm gnuplot gnuplot-mode helm-unicode elpy markdown-mode outline-magic yasnippet-classic-snippets material-theme graphviz-dot-mode nov csv-nav wgrep-ag nasm-mode web-mode yasnippet-snippets async helm auctex diminish helm-config helm-command helm-files ascii-art-to-unicode pyim yatemplate sicp org-pdfview org-chinese-utils htmlize helm-gtags helm-c-yasnippet helm-ag exec-path-from-shell csv-mode cider-eval-sexp-fu use-package anzu))
  '(pdf-annot-activate-created-annotations t t)
  '(pdf-cache-image-limit 32)
  '(pdf-isearch-batch-mode t t)
  '(pdf-view-max-image-width 2048)
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(pdf-view-resize-factor 1.8)
- '(read-mail-command (quote ignore))
+ '(read-mail-command 'ignore)
  '(recentf-max-menu-items 25)
  '(recentf-max-saved-items 25)
  '(recentf-mode t)
  '(recentf-save-file "~/.recentf")
  '(safe-local-variable-values
-   (quote
-	((org-export-latex-preamble . "\\documentclass[ngerman]{article}
+   '((From . TLPI:)
+	 (From . blp4e)
+	 (From . "tlpi, Listing 44-2")
+	 (Usage . "/tmp/a.out 'string'")
+	 (Ref . https://www\.arp242\.net/tmux\.html)
+	 (ref . "https://stackoverflow.com/questions/5311515/gcc-fpic-option")
+	 (org-export-latex-preamble . "\\documentclass[ngerman]{article}
 \\usepackage[T1]{fontenc}
 \\usepackage[utf8]{inputenc}
 \\usepackage{verbatim}
@@ -119,19 +117,18 @@
 \\usepackage{breakurl}
 \\usepackage{array}
 ")
-	 (reftex-default-bibliography . "ref.bib"))))
+	 (reftex-default-bibliography . "ref.bib")))
  '(scheme-program-name "guile")
  '(scroll-bar-mode nil)
  '(select-enable-clipboard t)
- '(setq (quote (RCS CVS SVN SCCS SRC Bzr Git Hg Mtn)) t)
+ '(setq '(RCS CVS SVN SCCS SRC Bzr Git Hg Mtn) t)
  '(shell-command-with-editor-mode t)
  '(tool-bar-button-margin 1 t)
  '(tool-bar-mode nil)
  '(track-eol t)
  '(unibyte-display-via-language-environment t)
  '(unicode-fonts-block-font-mapping
-   (quote
-	(("Arrows"
+   '(("Arrows"
 	  ("DejaVu Sans Mono" "Symbola" "Noto Sans Symbols"))
 	 ("Block Elements"
 	  ("DejaVu Sans Mono" "Noto Sans Symbols" "Symbola"))
@@ -200,15 +197,14 @@
 	 ("Supplementary Private Use Area-B"
 	  ("DejaVuSansMono Nerd Font" "NotoMono Nerd Font"))
 	 ("Transport and Map Symbols"
-	  ("DejaVuSansMono Nerd Font" "NotoMono Nerd Font" "Symbola")))))
+	  ("DejaVuSansMono Nerd Font" "NotoMono Nerd Font" "Symbola"))))
  '(uniquify-after-kill-buffer-p t)
- '(uniquify-buffer-name-style (quote reverse) nil (uniquify))
+ '(uniquify-buffer-name-style 'reverse nil (uniquify))
  '(uniquify-ignore-buffers-re "^\\*")
  '(uniquify-separator " @ ")
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
-   (quote
-	((20 . "#BC8383")
+   '((20 . "#BC8383")
 	 (40 . "#CC9393")
 	 (60 . "#DFAF8F")
 	 (80 . "#D0BF8F")
@@ -225,16 +221,16 @@
 	 (300 . "#7CB8BB")
 	 (320 . "#8CD0D3")
 	 (340 . "#94BFF3")
-	 (360 . "#DC8CC3"))))
+	 (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3")
- '(version-control (quote never))
+ '(version-control 'never)
  '(view-diary-entries-initially t)
  '(view-read-only t)
  '(visible-bell t)
  '(warning-minimum-level :emergency)
  '(warning-minimum-log-level :emergency)
- '(warning-suppress-types (quote ((\(undo\ discard-info\)))))
- '(web-mode-engines-alist (quote (("php" . "\\.phtml\\'") ("blade" . "\\.blade\\."))) t)
+ '(warning-suppress-types '((\(undo\ discard-info\))))
+ '(web-mode-engines-alist '(("php" . "\\.phtml\\'") ("blade" . "\\.blade\\.")) t)
  '(x-stretch-cursor t)
  '(yas-minor-mode nil t))
 (custom-set-faces
@@ -244,4 +240,4 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 240 :family "DejaVuSansMono Nerd Font"))))
  '(mode-line ((t (:height 160 :family "DejaVuSansMono Nerd Font"))))
- '(org-table ((t (:family "Noto Sans Mono CJK SC")))))
+ '(org-table ((t (:background "gray25" :foreground "light gray" :family "Noto Sans Mono CJK SC")))))
